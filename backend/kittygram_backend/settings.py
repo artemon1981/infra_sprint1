d89c9cd4392f67a86c9166bde56d9eadecac13c0
artemon1981 <ostrikovpashka@yandex.ru>
2024-02-26 12:31:05 +0000

    third

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -99,7 +99,7 @@ STATIC_URL = 'static_backend'
 STATIC_ROOT = BASE_DIR / 'static_backend' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/kittygram/media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
